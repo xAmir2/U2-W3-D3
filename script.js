ae6cad3ba1abb995399d847adafa4d94d3ae610a
Amir B. Kahna <amir.kahna2@gmail.com>
2026-04-15 16:30:16 +0200

    Adding cart section

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const apiUrl = "https://striveschool-api.herokuapp.com/books";
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 const getBooks = () => {
   fetch(apiUrl)
@@ -18,14 +19,22 @@ const getBooks = () => {
       const library = document.getElementById("library");
       library.innerHTML = "";
       for (let i = 0; i < data.length; i++) {
-        library.innerHTML += `<div class= "col-3 mb-4" id="bookCard${i}"><div class="card">
-  <img src=${data[i].img} class="card-img-top" alt="pic" height="350px">
-  <div class="card-body">
-    <h5 class="card-title">${data[i].title}</h5>
-    <p class="card-text">${data[i].category}<br/>${data[i].price}</p>
-    <a href="#" class="btn btn-primary" onclick="removeCard(${i})">Delete</a>
+        library.innerHTML += `
+<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4" id="bookCard${i}">
+  <div class="card h-100">
+    <img src="${data[i].img}" class="card-img-top" height="350px">
+    <div class="card-body d-flex flex-column">
+      <h5 class="card-title">${data[i].title}</h5>
+      <p class="card-text">${data[i].category}<br/>€${data[i].price}</p>
+      
+      <div class="mt-auto d-flex justify-content-between">
+        <button class="btn btn-danger" onclick="removeCard(${i})">Delete</button>
+        <button class="btn btn-success" onclick='addToCart(${JSON.stringify(
+          data[i],
+        )})'>Buy</button>
+      </div>
+    </div>
   </div>
-</div>
 </div>`;
       }
     })
@@ -41,3 +50,31 @@ const removeCard = (i) => {
     card.remove();
   }
 };
+
+const addToCart = (book) => {
+  cart.push(book);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  renderCart();
+};
+
+const renderCart = () => {
+  const cartList = document.getElementById("cart");
+  cartList.innerHTML = "";
+
+  cart.forEach((book, index) => {
+    cartList.innerHTML += `
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        ${book.title}
+        <button class="btn btn-sm btn-danger" onclick="removeFromCart(${index})">X</button>
+      </li>
+    `;
+  });
+};
+
+const removeFromCart = (index) => {
+  cart.splice(index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  renderCart();
+};
+
+renderCart();
